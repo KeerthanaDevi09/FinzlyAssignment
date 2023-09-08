e4876d8b10c3ec8a80ceff38ba191f0cc9793d94
Keerthana Devi <90337046+KeerthanaDevi09@users.noreply.github.com>
2023-09-08 09:19:23 +0530

    Delete FrontEnd/Typescript/Day04/task.ts

--- FrontEnd/Typescript/Day04/task.ts
+++
@@ -1,9 +0,0 @@
-//access a class in another class in outer  package 
-
-import {Employee,show,keyValue} from '../Day4/classfile'
-let obj1=new Employee(3,"Sreeja",60000,"HR");
-obj1.display();
-obj1.taxCalculation();
-
-show("Aruna","Rams");
-console.log(keyValue);
