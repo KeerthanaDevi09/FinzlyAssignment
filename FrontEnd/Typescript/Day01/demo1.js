34fe945563323748254fdc8889b3e77be61cf4c6
Keerthana Devi <90337046+KeerthanaDevi09@users.noreply.github.com>
2023-09-08 09:11:12 +0530

    Add files via upload

---
+++ FrontEnd/Typescript/Day01/demo1.js
@@ -0,0 +1,23 @@
+console.log("Hello World!!!");
+console.log('Hello World!!!');
+console.log("    Have \n    A\n    Nice\n    Day\n");
+var a = 10;
+console.log("Value of a is " + a);
+var a;
+a = 20;
+console.log("Value of a is " + a);
+var s1 = "keerthi";
+console.log("Value of s1 is " + s1);
+var s1;
+s1 = 'aruna';
+console.log("Value of s1 is " + s1);
+var t = true;
+console.log("Value of t is " + t);
+var t;
+t = false;
+console.log("Value of t is " + t);
+var b; //data type as well as value
+b = null;
+console.log("Value of b is " + b);
+var s;
+console.log("value of s is " + s);
---
+++ FrontEnd/Typescript/Day01/demo1.ts
@@ -0,0 +1,32 @@
+console.log("Hello World!!!");
+console.log('Hello World!!!');
+console.log(`    Have 
+    A
+    Nice
+    Day
+`);
+
+var a:any=10;
+console.log("Value of a is "+a);
+var a!:any;
+a=20;
+console.log("Value of a is "+a);
+
+var s1:string="keerthi";
+console.log("Value of s1 is "+s1);
+var s1!:string;
+s1='aruna';
+console.log("Value of s1 is "+s1);
+
+var t:boolean=true;
+console.log("Value of t is "+t);
+var t!:boolean;
+t=false;
+console.log("Value of t is "+t);
+
+var b !:number ; //data type as well as value
+b=11;
+console.log("Value of b is "+b);
+
+var s!:string | number; // take string value as well as number
+console.log("value of s is "+s);
