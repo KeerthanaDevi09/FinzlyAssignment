921e6bda8836b8abc9947aba50705e5844f6e772
Keerthana Devi <90337046+KeerthanaDevi09@users.noreply.github.com>
2023-09-25 00:09:06 +0530

    Delete FinalProject/BackEnd/src directory

--- FinalProject/BackEnd/src/app/model/customer.ts
+++
@@ -1,9 +0,0 @@
-export interface Customer{
-   customerId:number;
-   customerName:string;
-   customerGmail:string;
-   dueDate:string;
-   customerMobileNumber:number;
-   otp:string;
-
-}
--- FinalProject/BackEnd/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl1:"http://localhost:8084/employee/",
-  baseUrl2:"http://localhost:8084/customer/",
-  baseUrl3:"http://localhost:8084/bill",
-  baseUrl4:"http://localhost:8084/transaction"
-};
